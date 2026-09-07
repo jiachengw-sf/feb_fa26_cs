a182eabbe06f1629e6800f9bce8b240e236c00c6
Jiacheng <jiacheng.wen.sf@gmail.com>
2026-09-07 14:55:33 -0700

    Upload API setup

--- back_end/main.py
+++ back_end/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from fastapi import UploadFile, File
 
 app = FastAPI()
 
@@ -13,3 +15,16 @@ app.add_middleware(
 @app.get("/health")
 def health_check():
     return {"status": "backend is running"}
+
+UPLOAD = "uploads"
+os.makedirs(UPLOAD, exist_ok=True)
+
+@app.post("/upload")
+async def upload_csv(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD, file.filename)
+
+    contents = await file.read()
+    with open(file_path, "wb") as f:
+        f.write(contents)
+
+    return {"filename": file.filename, "size_bytes": len(contents)}
